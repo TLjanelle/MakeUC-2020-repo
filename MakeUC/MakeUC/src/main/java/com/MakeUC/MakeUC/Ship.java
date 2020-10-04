d050d816d3351dae1699d1388c112a177a40ffb9
TLjanelle <44989804+TLjanelle@users.noreply.github.com>
2020-10-03 21:55:09 -0400

    21.55

--- MakeUC/MakeUC/src/main/java/com/MakeUC/MakeUC/Ship.java
+++ MakeUC/MakeUC/src/main/java/com/MakeUC/MakeUC/Ship.java
@@ -1,22 +1,42 @@
 package com.MakeUC.MakeUC;
 
-public class Ship{
-    public String name;
-    public String type;
-    public String[] roles;
-    public boolean active;
-    public int weightlb;
-    public int weightkg;
-    public int year;
-    public String port;
-    public String[] missions;
-    public String url;
-    public String image;
+public class Ships{
 
-    public Ship() { }
+    public String ShipId;
+    public String ShipName;
+    public String ShipModel;
+    public String ShipType;
+    public String Role;
+    public int YearBuilt;
+    public String HomePort;
+    public String Status;
+    public int  Speed;
+    public int Course;
+    public int SuccessfulLandings;
+    public int AttemptedLanding;
+    public String Mission;
 
-    // public void getLocation(float[][] ){
+    Ships(String a, String b, String c, String d, String e
+    int f, String g, String h, int i, int, j, int k, int l, String m)
+    {
+        ShipId = a;
+        ShipName = b;
+        ShipModel = c;
+        ShipType = d;
+        Role = e;
+        YearBuilt = f;
+        HomePort = g;
+        Status = h;
+        Speed = i;
+        Course = j;
+        SuccessfulLandings = k;
+        AttemptedLanding = l;
+        Mission = m;
 
-    // }
+    }
+
+    public void ShipInfo(){
+        
+     }
 
 }
