2ba72300c8e1911de64f1c972b730e32118f02ff
TLjanelle <44989804+TLjanelle@users.noreply.github.com>
2020-10-03 21:15:22 -0400

    Commit21.15

--- MakeUC/MakeUC/src/main/java/com/MakeUC/MakeUC/Ships.java
+++ MakeUC/MakeUC/src/main/java/com/MakeUC/MakeUC/Ships.java
@@ -2,14 +2,24 @@ package com.MakeUC.MakeUC;
 
 public class Ships{
 
-    public float lattitude;
-    public float longitude;
+    public String ShipId;
+    public String ShipName;
+    public String ShipModel;
+    public String ShipType;
+    public String Role;
+    public int YearBuilt;
+    public String HomePort;
+    public String Status;
+    public int  Speed;
+    public int Course;
+    public int SuccessfulLandings;
+    public int AttemptedLanding;
+    public String Mission;
 
-    public Ships(float lat, float lon)
+    public Ships(String a, String b, String c, String d, String e
+    int f, String g, String h, int i, int, j, int k, int l, String m)
     {
-        lattitude = lat;
-        longitude = lon;
-    }
+        
 
     // public void getLocation(float[][] ){
 
